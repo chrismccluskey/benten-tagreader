342e037acce0077418d87fd45368a3f005b9348a
Chris McCluskey <cmccluskey@digitaltrends.com>
2018-12-15 23:57:01 -0800

    Switch from id3-go to tag. Build specific prototype tool for solving specific problem.

--- main.go
+++ main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
-	"github.com/mikkyang/id3-go"
+	"github.com/dhowden/tag"
 	"path/filepath"
 )
 
 func main() {
-	fmt.Println("Reading files in directory")
 	var files []string
-	var id3v2Frames = []string{"AENC","APIC","ASPI","COMM","COMR","ENCR","EQU2","ETCO","GEOB","GRID","LINK","MCDI","MLLT","OWNE","PRIV","PCNT","POPM","POSS","RBUF","RVA2","RVRB","SEEK","SIGN","SYLT","SYTC","TALB","TBPM","TCOM","TCON","TCOP","TDEN","TDLY","TDOR","TDRC","TDRL","TDTG","TENC","TEXT","TFLT","TIPL","TIT1","TIT2","TIT3","TKEY","TLAN","TLEN","TMCL","TMED","TMOO","TOAL","TOFN","TOLY","TOPE","TOWN","TPE1","TPE2","TPE3","TPE4","TPOS","TPRO","TPUB","TRCK","TRSN","TRSO","TSOA","TSOP","TSOT","TSRC","TSSE","TSST","TXXX","UFID","USER","USLT","WCOM","WCOP","WOAF","WOAR","WOAS","WORS","WPAY","WPUB","WXXX"}
 
 	root := "./"
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
@@ -24,23 +23,22 @@ func main() {
 	for _, file := range files {
 		if len(file) > 3 {
 			if file[len(file)-3:len(file)] == "mp3" {
-				fmt.Println(file)
-				mp3File, err := id3.Open(file)
-				defer mp3File.Close()
 
+				fileContents, err := os.Open(file) // For read access.
 				if err != nil {
-					fmt.Println(err)
+					log.Fatal(err)
 				}
 
-				for _, frameName := range id3v2Frames {
-					id3Frame := mp3File.Frame(frameName)
-					if id3Frame != nil {
-						fmt.Printf("%s: %s\n", frameName, id3Frame.String())
-					} else {
-						fmt.Printf("%s: (empty)\n", frameName)
+				m, err := tag.ReadFrom(fileContents)
+				if err != nil {
+					log.Fatal(err)
+				}
+				for k, v := range m.Raw() {
+					if k == "TIT1" && v == "DELETE" {
+						//fmt.Printf("%s: %s\n", k, v)
+						fmt.Println(file)
 					}
 				}
-				fmt.Println()
 			}
 		}
 	}

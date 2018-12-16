fa47918124c013d29703665606b5fb135af77ca6
Chris McCluskey <cmccluskey@digitaltrends.com>
2018-12-16 13:03:30 -0800

    Create prototype tool to fix specific purpose

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"github.com/dhowden/tag"
 	"path/filepath"
+
+	"github.com/dhowden/tag"
+	"gopkg.in/alessio/shellescape.v1"
 )
 
 func main() {
@@ -22,21 +24,28 @@ func main() {
 
 	for _, file := range files {
 		if len(file) > 3 {
-			if file[len(file)-3:len(file)] == "mp3" {
+			if file[len(file)-4:len(file)] == ".mp3" {
 
 				fileContents, err := os.Open(file) // For read access.
 				if err != nil {
-					log.Fatal(err)
-				}
+					log.Print(err)
+				} else {
 
-				m, err := tag.ReadFrom(fileContents)
-				if err != nil {
-					log.Fatal(err)
-				}
-				for k, v := range m.Raw() {
-					if k == "TIT1" && v == "DELETE" {
-						//fmt.Printf("%s: %s\n", k, v)
-						fmt.Println(file)
+					m, err := tag.ReadFrom(fileContents)
+					if err != nil {
+						log.Print(err)
+					} else {
+
+						//fmt.Println(file)
+						for k, v := range m.Raw() {
+
+							//fmt.Printf("%s: %s\n", k, v)
+
+							if k == "TIT1" && v == "DELETE" {
+								fmt.Println(shellescape.Quote(file))
+							}
+						}
+						//fmt.Println()
 					}
 				}
 			}
